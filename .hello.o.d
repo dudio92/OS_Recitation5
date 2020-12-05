35fb210b6fc1eb1a3964172f61af9c11c7bac139
David <dudioren92@gmail.com>
2020-12-05 13:46:04 +0200

    Initial commit

---
+++ .hello.o.d
@@ -0,0 +1,4 @@
+compiler-file3976604299396159407.o: /tmp/compiler-file3976604299396159407 \
+ include/linux/kconfig.h include/generated/autoconf.h \
+ include/linux/compiler_types.h include/linux/compiler_attributes.h \
+ include/linux/compiler-gcc.h
